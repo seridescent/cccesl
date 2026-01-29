fbc82581147a3ea165650ba7afc888dc24c1ca6b
seridescent <seridescent@gmail.com>
2026-01-29 17:52:19 -0500

    nix package

--- flake.nix
+++ flake.nix
@@ -9,7 +9,20 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { pkgs, ... }: {
+      perSystem = { lib, pkgs, ... }: {
+        packages.default = pkgs.buildGo125Module {
+          pname = "cccesl";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = null;
+
+          meta = {
+            description = "claude code cache expiry statusline";
+            homepage = "https://github.com/seridescent/cccesl";
+            license = lib.licenses.mit;
+          };
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.go_1_25
